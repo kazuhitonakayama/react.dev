09bd09c717f86d92b623bd3dd00dcd7188c9f86d
KazuhitoNakayama <firstjin19990101@gmail.com>
2024-01-08 15:36:34 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+export default function Square() {
+  const alertNumber = (event) => {
+    console.log(event.target)
+  }
+
+
+  const tiles = [
+    { id: 1 },
+    { id: 2 },
+    { id: 3 },
+    { id: 4 },
+    { id: 5 },
+    { id: 6 },
+    { id: 7 },
+    { id: 8 },
+    { id: 9 }
+  ]
+
+  const buttons = tiles.map(tile => {
+    return (
+      <button className="square" key={tile.id} onClick={alertNumber} ></button>
+    )
+  })
+
+  return (
+    <>
+      <div class="squares-wrapper">
+        {buttons}
+      </div>
+    </>
+  )
+}
